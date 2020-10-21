5450b3dd0b58a9c959b318b36bf7f2593ed7ced8
Carlos Perez <carlosperortega14@gmail.com>
2020-10-20 22:52:23 -0500

    Agregando funcion Palabras

--- Palabras.py
+++ Palabras.py
@@ -1,3 +1,32 @@
-def palabras():
-    print ("Palabras esta viva")
+import random
 
+
+#Ingresa una dificultad para seleccionar una categoría de palabras
+palabras_categoria = {'easy':['mamey','carro','perro'],'medium':['portaaviones','aracnido','turbulencia'],'hard':['onomatopeya','civilizacion','canasta']}
+
+ 
+
+def palabras():
+       
+    dificultad = input("Write the difficulty: easy, medium or hard: ")
+    while True:
+        if dificultad == 'easy':
+            x = palabras_categoria['easy'][random.randrange(0,3)]
+            return x
+            break
+        elif dificultad == 'medium':
+            x = palabras_categoria['medium'][random.randrange(0,3)]
+            return x
+            break
+        elif dificultad == 'hard':
+            x = palabras_categoria['hard'][random.randrange(0,3)]
+            return x
+            break
+        
+        else:
+            print("Must be easy, medium or hard")
+
+    
+
+y = palabras()
+print(y)
